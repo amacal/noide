60fcdce89ddd776eee420fb4345913ba9fdef961
Adrian Macal <adma@amacal.pl>
2015-03-02 20:20:09 +0100

    Change visible components names.

--- src/noide.engine/ICompiler.cs
+++ src/noide.engine/ICompiler.cs
@@ -1,9 +1,25 @@
 using System;
+using System.Collections.Generic;
 
 namespace noide
 {
 	public interface ICompiler
 	{
 		IResource Compile(ICompilable target);
+	}
+
+	public interface ICompilable
+	{
+		String Name { get; }
+
+		String Output { get; }
+
+		String Type { get; }
+
+		IReadOnlyCollection<String> GetSources();
+
+		IReadOnlyCollection<String> GetReferences();
+
+		IReadOnlyCollection<String> GetResources();
 	}
 }
